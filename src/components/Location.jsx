b630948737f6be4da94edb1e366f3e8458d7cfc9
Gerenciamento-de-Veiculos <pedrohenriquemenezes76@gmail.com>
2025-11-27 06:18:09 -0300

    corrigindo link da localização

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -6,7 +6,7 @@ import locationImage from "../assets/location.jpg";
 export default function Location() {
   const address = "RUA JABOTICABAL, QD 82 LT20, Luziânia - GO";
 
-  const googleMapsUrl = `-16.150226, -47.954381${encodeURIComponent(
+  const googleMapsUrl = `https://www.google.com/maps/place/R.+Jaboticabal,+303-1+-+Jardim+do+Inga,+Luzi%C3%A2nia+-+GO,+72850-300/@-16.1507719,-47.9539207,17z/data=!3m1!4b1!4m9!1m2!2m1!1srua+jaboticabal+quadra+82+lote+20+72850300!3m5!1s0x93599adc59c5265d:0x6075dd31fbc41fb9!8m2!3d-16.1507719!4d-47.9539207!16s%2Fg%2F11g623j7_p?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D${encodeURIComponent(
     address
   )}`;
 
